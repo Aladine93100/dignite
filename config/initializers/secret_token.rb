4a5c08d8685d837b2c3cca876c35e92805b99c9c
Aladine <alazaiane@yahoo.fr>
2014-06-01 17:45:23 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dignite::Application.config.secret_key_base = '5a5f94eb5d4bf4c3ff4f92510572ea6ff4669d08662c89a333e23c9926ea845d65e5566cfd44467c2ffc7c1d2ac2bca7089598457da7d566ec90dd49d6ed1e03'
